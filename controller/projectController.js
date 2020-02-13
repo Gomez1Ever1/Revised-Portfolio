607f66f4132a90ff096142dc1e6b95da7fcce5c8
Ever's PC <gomezsantiagoeverardo@gmail.com>
2020-02-12 21:15:16 -0800

    finally finished logic of backend/handlebars, will work on styling to make site fully employer competitive

--- controller/projectController.js
+++ controller/projectController.js
@@ -3,17 +3,40 @@ const router = express.Router();
 const db = require("../models");
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function (req, res) {
-    db.Project
-        .findAll({
-            attributes: {
-                exclude: [
-                    "createdAt",
-                    "updatedAt"
-                ]
-            }
-        })
+    getProjects()
         .then(function (data) {
-            res.render("index", { project: data });
+            const allProjects = data.map(project => {
+                return new objToOwnProp(project.dataValues);
+            });
+            allProjects[0].className = "active";
+            console.log(allProjects)
+            res.render("index", { project: allProjects });
         });
 });
+function getProjects() {
+    return new Promise((resolve, reject) => {
+        db.Project
+            .findAll({
+                model: db.project,
+                attributes: {
+                    exclude: [
+                        "createdAt",
+                        "updatedAt"
+                    ]
+                }
+            })
+            .then(projects => {
+                resolve(projects);
+            }
+            )
+            .catch(err => { reject(err); }
+            )
+    })
+};
+var objToOwnProp = function (newModel) {
+    this.project_name = newModel.project_name;
+    this.link = newModel.link;
+    this.image_link = newModel.image_link;
+    this.className = "";
+};
 module.exports = router;
